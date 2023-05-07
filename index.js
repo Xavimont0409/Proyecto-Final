8464dd76c45fb68a9bc27f0cb2ca3db981648138
lean0mar <lm.martinez.leandro@gmail.com>
2023-05-07 19:47:35 -0300

    lean0mar

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function laCajaDePandora(numero){
 // proximamente escribiremos codigo aqui
-}
+    if (numero % 2 === 0) {
+      return numero.toString(2); // convierte a binario
+    } else {
+      return numero.toString(16); // convierte a hexadecimal
+    }
+  }
+
+  function Leandro() {
+    return {
+    name: 'Leandro',
+    age: '18',
+    nationality: 'Argentina'
+    }
+    }
+    
