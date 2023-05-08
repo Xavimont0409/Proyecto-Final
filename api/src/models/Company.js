fb4e669db18574024c743b7ab28a4fb5bcf8bf04
lean0mar <lm.martinez.leandro@gmail.com>
2023-05-08 12:19:15 -0300

    Modelo de Company Finalizado

--- api/src/models/Company.js
+++ api/src/models/Company.js
@@ -3,19 +3,44 @@ const {DataTypes} = require('sequelize')
 module.exports = (sequelize) => {
     sequelize.define('Company', {
         id:{
-
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            unique: true
         },
         name: {
-
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        company_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        business_name: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        ruc: {
+            type: DataTypes.STRING(13),
+            allowNull: false,
+            unique: true,
         },
         cuit: {
-
+            type: DataTypes.STRING(11),
+            allowNull: false,
+            unique: true,
         },
         email: {
-
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+                }
         },
         phone: {
-
+            type: DataTypes.STRING(20),
+            allowNull: false,
         },
     } , {
         timestamps: false
