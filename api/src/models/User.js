1cbb60ad07da73045f74083d350605aef55ba949
lean0mar <lm.martinez.leandro@gmail.com>
2023-05-08 12:26:24 -0300

    Modelo de User Finalizado

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -3,16 +3,30 @@ const {DataTypes} = require('sequelize')
 module.exports = (sequelize) => {
     sequelize.define('User', {
         id:{
-
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            unique: true
         },
         name: {
-
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false
         },
         email: {
-
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
         },
         phone: {
-
+            type: DataTypes.STRING(20),
+            allowNull: false
         },
     } , {
         timestamps: false
