e80f8501b9707047b5d32bbd0fc5e60de507588a
Andres Felipe Rojas Perez <you@example.com>
2023-05-08 23:04:09 -0500

    maquetado(client/views/EmpleoDetail): maquetado de la vista Empleo Detail

--- client/src/views/EmpleoDetail/EmpleoDetail.jsx
+++ client/src/views/EmpleoDetail/EmpleoDetail.jsx
@@ -1,3 +1,6 @@
+import style from "./EmpleoDetail.module.css";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 
 
@@ -6,7 +9,31 @@ const EmpleoDetail = () => {
 
 
     return(
-        <h1>ESTA ES LA VISTA DE EMPLEO-DETAIL</h1>
+    <div className={style.mainContainer}>
+        <h1>ACÁ VA LA NAVBAR</h1>
+        <div className={style.mainContainer2}>
+            <div className={style.containerEmpleosRel}>
+                Acá van las card de empleos relacionados
+            </div>
+            <div className={style.mainContainer3}>
+            <Tabs
+            defaultActiveKey="profile"
+            id="justify-tab-example"
+            className="mb-3"
+            justify
+            >
+             <Tab eventKey="home" title="Home">
+             <div className={style.containerDetail}>ACÁ VA LA CARD DE DETALLE DEL EMPLEO</div>
+            </Tab>
+            <Tab eventKey="profile" title="Profile">
+             <div className={style.containerDetail}>ACÁ VA LA CARD DE DETALLE DE LA EMPRESA</div>
+            </Tab>
+            </Tabs>
+            </div>
+        </div>
+
+    </div>
+
     );
 };
 
