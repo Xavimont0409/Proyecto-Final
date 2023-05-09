4a1dc36445dadeb1136cd2401bd59808af3ae576
Augusto <augusto.viggiano@gmail.com>
2023-05-09 14:13:59 -0300

    landing hecha/falta css

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,21 +1,27 @@
 import style from './Landing.module.css';
-import { Button } from 'react-bootstrap';
 import img from './viejo feliz.jpg';
+import NavBar from '../../components/NavBar/NavBar';
 
 const Landing = () => {
 
     return (
-        <div className='{style.landingDiv}'>
-            <nav></nav>
-            <div className='{style.imgDiv}'>
-                <img src={img} className='{style.img}'/>
+        <div className={style.landingDiv}>
+            <div className={style.navDiv}>
+                <NavBar/>
             </div>
-            <div className='{style.searchDiv}'>
-                <input className='{style.input}' placeholder=''/>
-                <Button>Busca Tu Trabajo</Button>
-            </div>
-            <div>
-                <p>Somos la mejor aplicacion loco, usanos</p>
+            <div className={style.containerDiv}>
+                <div className={style.imgDiv}>
+                    <img src={img} className={style.img}/>
+                </div>
+                <div className={style.decorationDiv}>
+                    <div className={style.searchDiv}>
+                        <input className={style.input} placeholder=''/>
+                        <button className="btn btn-outline-success me-2" type="button">Busca tu trabajo</button>
+                    </div>
+                    <div className={style.paragraph}>
+                        <p className={style.text}>Somos la mejor aplicacion loco, usanos</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
