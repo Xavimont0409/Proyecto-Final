fbb1d40fb416c42e25ee01214d8a24b038ec75a2
Victor <henry079@gmail.com>
2023-05-10 14:37:08 -0500

    create views Form

--- client/src/components/CardEmpleo/CardEmpleo.jsx
+++ client/src/components/CardEmpleo/CardEmpleo.jsx
@@ -1,22 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link} from "react-router-dom"
+
 import style from './CardEmpleo.module.css'
 
 
 
 const CardEmpleo = (props) => {
 
-
-    return (
+   
+return (
 
 
         <Link className={style.words} to={`/empleoDetail/${props.id}`}>
             <div className={style.mainContainer}>
                 <div className={style.cardContainer}>
-                    <img className={style.image} src={props.logo} alt="logo" />
+                    {/* <img className={style.image} src={props.logo} alt="logo" /> */}
                     <h2 className={style.temps} >{props.title}</h2>
                 </div>
                 <p>{props.description}</p>
             </div>
+
 
         </Link>
   )
--- client/src/views/index.js
+++ client/src/views/index.js
@@ -5,5 +5,8 @@ import LandingEmpresa from "./LandingEmpresa/LandingEmpresa";
 import Register from "./Register/Register";
 import IniciarSesion from "./IniciarSesion/IniciarSesion";
 import FormRegister from "./IniciarSesion/FormRegister";
+import FormCv from "./FormCv/FormCv";
+import FormVacante from "./FormVacante/FormVacante";
+import FormEmpresa from "./FormEmpresa/FormEmpresa";
 
-export {EmpleoDetail, Empleos, Landing, LandingEmpresa, Register, IniciarSesion, FormRegister}
+export {EmpleoDetail, Empleos, Landing, LandingEmpresa, Register, IniciarSesion, FormRegister, FormCv, FormVacante, FormEmpresa}
