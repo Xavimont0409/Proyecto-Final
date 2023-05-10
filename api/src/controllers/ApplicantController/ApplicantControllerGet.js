de4d70d3d83bb1ed82694b981236abc6a4550278
Xavimont0409 <111645686+Xaviermon@users.noreply.github.com>
2023-05-10 15:35:59 -0500

    Cambios en handlres y controllers, agregando relacion applicant y cv By:xavier

--- api/src/controllers/ApplicantController/ApplicantControllerGet.js
+++ api/src/controllers/ApplicantController/ApplicantControllerGet.js
@@ -3,7 +3,11 @@ const { Applicant, Cv } = require('../../db')
 const allApplicant = async() =>{
   return await Applicant.findAll({include: {model: Cv}})
 }
+const allApplicantId = async(id) =>{
+  return await Applicant.findAll({include: {model: Cv}, where: {id: id}})
+}
 
 module.exports = {
   allApplicant,
+  allApplicantId
 }
--- api/src/controllers/CvController/CvControllerPost.js
+++ api/src/controllers/CvController/CvControllerPost.js
@@ -1,7 +1,7 @@
 const { Cv } = require('../../db');
 
-const createCv = async (dni, name, lastName, address, photo, profesion, github, linkedin, work_experience, personal_description, education) => {
-  const newCv = await Cv.create({ dni, name, lastName, address, photo, profesion, github, linkedin,  work_experience, personal_description, education });
+const createCv = async (dni, name, lastName, address, photo, profession, github, linkedin, work_experience, personal_description, education, ApplicantId) => {
+  const newCv = await Cv.create({ dni, name, lastName, address, photo, profession, github, linkedin,  work_experience, personal_description, education, ApplicantId });
 
   return newCv
 };
