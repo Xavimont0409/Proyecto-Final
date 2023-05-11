0303d37d77d64359ea19bd88cf4c45854ed5b2b2
Augusto <augusto.viggiano@gmail.com>
2023-05-11 04:44:31 -0300

    Loading, responsive, botones, Auth0 funcionando en iniciar secion

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,18 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './Redux/store';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-    <App />
+      <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
+        <App />
+      </Auth0Provider>
     </BrowserRouter>
   </Provider>
-  
 );
--- client/src/views/Empleos/Empleos.jsx
+++ client/src/views/Empleos/Empleos.jsx
@@ -1,26 +1,36 @@
-import NavBar from "../../components/NavBar/NavBarUnlog"
+import NavBar from "../../components/NavBar/NavBarUnlog";
 import CardsContainerEmpleo from "../../components/CardsContainerEmpleo/CardsContainerEmpleo";
-import Filter from "../../components/Filter/Filter"
-import style from "./Empleos.module.css"
+import Filter from "../../components/Filter/Filter";
+import Loading from "../../components/Loading/Loading";
+import style from "./Empleos.module.css";
 import { useSelector } from "react-redux";
-
+import { useEffect, useState } from "react";
 
 
 const Empleos = () => {
 
+    const [isLoading, setIsLoading] = useState(true);
     const currentCard = useSelector(state => state.publications)
     console.log(currentCard);
-    
-    
- 
 
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+    }, []);
+
+    if (isLoading) {
+        return <Loading />;
+    }
 
     return (
         <div className={style.mainContainer}>
             <NavBar></NavBar>
             <div className={style.filterAndCardsContainer}>
-                <Filter></Filter>
-                <div>
+                <div className={style.filters}>
+                    <Filter></Filter>
+                </div>
+                <div className={style.cardsDiv}>
 
                     <CardsContainerEmpleo
                         currentCard={currentCard} />
@@ -29,7 +39,7 @@ const Empleos = () => {
             </div>
         </div>
     )
-}
+};
 
 
 export default Empleos;
