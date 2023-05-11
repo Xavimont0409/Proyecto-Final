a6880c72d444865886d33726981a856c77656b55
JuanCruz <juancruzgenovese22@gmail.com>
2023-05-11 18:33:15 -0300

    nungun cambio que afecte el flujo de la app

--- client/src/index.js
+++ client/src/index.js
@@ -20,7 +20,7 @@ root.render(
         domain={domain}
         clientId={clientId}
         authorizationParams={{
-          redirect_uri: window.location.href
+          redirect_uri: window.location.origin
         }}
       >
         <App />
