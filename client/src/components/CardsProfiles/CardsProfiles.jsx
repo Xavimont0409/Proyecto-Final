177663f5da9c8fa9b4045689d83b0fa308250690
Andres Felipe Rojas Perez <you@example.com>
2023-05-12 12:19:40 -0500

    compilación_trabajo_de_Augusto_Andres_Profiles

--- client/src/components/CardsProfiles/CardsProfiles.jsx
+++ client/src/components/CardsProfiles/CardsProfiles.jsx
@@ -1,21 +1,33 @@
 import style from './CardsProfiles.module.css';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { BsLinkedin } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
-const CardProfile = ({id, name, email, skills, experience}) => {
+const CardProfile = ({id, image, name, profession, description, skills, dni, phone, email, seniority, linkedin}) => {
     return (
-        <div className="" key={id}>
-            <div className="">
-                <p>{name}</p>
-            </div>
-            <div className=""> 
-                <p>{email}</p>
-            </div>
-            <div className="">
-                <p>{skills?.map(skill => { return <div className="">{skill}</div> })}</p>
-            </div>
-            <div className="">
-                <p>{experience}</p>
-            </div>
+        <div className={style.mainContainer} >
+        <div className={style.containerImgEmpresa}>
+        <img className={style.img} variant="top" src={image}/>
         </div>
+        <Card.Body >
+          <Card.Title className={style.title}>{name}</Card.Title>
+          <Card.Text className={style.text}>{profession}</Card.Text>
+        </Card.Body>
+        <ListGroup className={style.infoContainer}>
+          <ListGroup.Item className={style.item}>{description} </ListGroup.Item>
+          <ListGroup.Item className={style.item}>Skills: {skills?.map(skill => { return <div className={style.skills}>{skill}</div> })}</ListGroup.Item>
+          <ListGroup.Item className={style.item}>DNI : {dni}</ListGroup.Item>
+          <ListGroup.Item className={style.item}>Teléfono: {phone}</ListGroup.Item>
+          <ListGroup.Item className={style.item}>Email: {email}</ListGroup.Item>
+          <ListGroup.Item className={style.item}>Seniority: {seniority}</ListGroup.Item>
+        </ListGroup>
+        <Card.Body>
+          <Link to={linkedin}>
+          <BsLinkedin className={style.linkedin}/>
+          </Link>
+          </Card.Body>
+      </div>
     )
 };
 
