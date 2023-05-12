4cca5b5b8ca08d6a18742bd5aa6086f5cea20940
Xavimont0409 <111645686+Xaviermon@users.noreply.github.com>
2023-05-12 13:01:40 -0500

    reducer creado, agregado de los estados globales al reducer by: xavier

--- client/src/Redux/Reducer/reducer.js
+++ client/src/Redux/Reducer/reducer.js
@@ -1,144 +1,84 @@
+import {
+  GET_ALL_COMPANYS,
+  GET_ALL_USERS,
+  GET_ALL_VACANTS,
+  GET_COMPANY_DETAIL,
+  GET_USERS_DETAIL,
+  GET_VACANT_DETAIL,
+  POST_COMPANY,
+  POST_CV,
+  POST_USER,
+  POST_VACANT,
+} from "../Actions/actions-types/action-types";
+
 const initialState = {
-    publications: [
-        {
-            id: 1,
-            title: "DEVELOPER TRAINE",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Nocturna",
-            SeniorityId: "Traine",
-            WorkMethodId: "Presencial"
-        },
-        {
-            id: 2,
-            title: "DEVELOPER JUNIOR",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Nocturna",
-            SeniorityId: "Junior",
-            WorkMethodId: "Presencial"
-        },
-        {
-            id: 3,
-            title: "DEVELOPER SEMI-SENIOR",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Diurna",
-            SeniorityId: "Semi-Senior",
-            WorkMethodId: "Remoto"
-        },
-        {
-            id: 4,
-            title: "DEVELOPER SENIOR",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Diurna",
-            SeniorityId: "Senior",
-            WorkMethodId: "Remoto"
-        },
-        {
-            id: 5,
-            title: "FULL STACK WEB DEVELOPER",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Diurna",
-            SeniorityId: "Junior",
-            WorkMethodId: "Remoto"
-        },
-        {
-            id: 6,
-            title: "Administrador de empresas Junior",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Diurna",
-            SeniorityId: "Junior",
-            WorkMethodId: "Presencial"
-        },
-        {
-            id: 7,
-            title: "Gerente General",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Diurna",
-            SeniorityId: "Senior",
-            WorkMethodId: "Remoto"
-        },
-        {
-            id: 8,
-            title: "Administrador de empresas",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Nocturna",
-            SeniorityId: "Semi-Senior",
-            WorkMethodId: "Presencial"
-        },
-        {
-            id: 9,
-            title: "Gerente General",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Diurna",
-            SeniorityId: "Semi-Senior",
-            WorkMethodId: "Presencial"
-        },
-        {
-            id: 10,
-            title: "Gerente de Sucursal",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Diurna",
-            SeniorityId: "Semi-Senior",
-            WorkMethodId: "Remoto"
-        },
-        {
-            id: 11,
-            title: "Gerente de punto",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Diurna",
-            SeniorityId: "Traine",
-            WorkMethodId: "Presencial"
-        },
-        {
-            id: 12,
-            title: "Gerente de punto",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Diurna",
-            SeniorityId: "Junior",
-            WorkMethodId: "Presencial"
-        },
-        {
-            id: 13,
-            title: "Gerente Regional",
-            description: "buen sueldo, mal trato, peor paga",
-            createdAt: "2023-05-09T22:34:43.806Z",
-            updatedAt: "2023-05-09T22:34:43.806Z",
-            JourneyId: "Diurna",
-            SeniorityId: "Senior",
-            WorkMethodId: "Remoto"
-        },
-    ]
-  
+  Companys: [],
+  CompanyDetail: {},
+
+  Users: [],
+  UserDetail: {},
+
+  Vacant: [],
+  VacantDetail: {},
 };
 
 const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        default:
-            return { ...state }
-    }
+  switch (action.type) {
+    case GET_ALL_COMPANYS:
+      return{
+        ...state,
+        Company : action.payload,
+      }
+    case GET_COMPANY_DETAIL:
+      return{
+        ...state,
+        CompanyDetail : action.payload,
+      }
+    case GET_ALL_USERS:
+      return{
+        ...state,
+        Users : action.payload,
+      }
+    case GET_USERS_DETAIL:
+      return{
+        ...state,
+        UserDetail : action.payload,
+      }
+    case GET_ALL_VACANTS:
+      return {
+        ...state,
+        Vacant : action.payload,
+      }
+    case GET_VACANT_DETAIL:
+      return {
+        ...state,
+        VacantDetail : action.payload,
+      }
+      
+      
+//! LOS CASOS POST TAMBIEN SE TRAEN AL REDUCER POR BUENAS PRACTICAS
+    case POST_COMPANY:
+      return {
+        ...state,
+      }
+    case POST_USER:
+      return{
+        ...state,
+      }
+    case POST_CV:
+      return{
+        ...state,
+      }
+    case POST_CV:
+      return{
+        ...state,
+      }      
+
+    default:
+      return {
+        ...state,
+      };
+  }
 };
 
 export default rootReducer;
