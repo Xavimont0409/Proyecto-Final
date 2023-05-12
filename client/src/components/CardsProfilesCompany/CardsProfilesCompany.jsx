19b7a2f16b5770889de855d7a8d3d1ae8366f13c
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-05-12 15:06:52 -0300

    Update CardsProfilesCompany.jsx
    Updated Cards component for company to match the cards component for users

--- client/src/components/CardsProfilesCompany/CardsProfilesCompany.jsx
+++ client/src/components/CardsProfilesCompany/CardsProfilesCompany.jsx
@@ -1,17 +1,27 @@
-import style from './CardsProfiles.module.css';
+import style from './CardsProfilesCompany.module.css';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { BsLinkedin } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
-const CardProfileCompany = ({id, business_name, email, country}) => {
+const CardProfileCompany = ({ /*image,*/ business_name, country, email }) => {
     return (
-        <div className="" key={id}>
-            <div className="">
-                <p>{business_name}</p>
-            </div>
-            <div className=""> 
-                <p>{country}</p>
-            </div>
-            <div className="">
-                <p>{email}</p>
-            </div>
+        <div className={style.mainContainer} >
+            {/* <div className={style.containerImgEmpresa}>
+                <img className={style.img} variant="top" src={image} />
+            </div> */}
+            <Card.Body >
+                <Card.Title className={style.title}>{business_name}</Card.Title>
+            </Card.Body>
+            <ListGroup className={style.infoContainer}>
+                <ListGroup.Item className={style.item}>{country} </ListGroup.Item>
+                <ListGroup.Item className={style.item}>{email} </ListGroup.Item>
+            </ListGroup>
+            {/* <Card.Body>
+                <Link to={linkedin}>
+                    <BsLinkedin className={style.linkedin} />
+                </Link>
+            </Card.Body> */}
         </div>
     )
 };
