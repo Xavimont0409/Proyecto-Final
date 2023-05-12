04a2a8574030bb0e41537120695b2d87e802f190
Xavimont0409 <111645686+Xaviermon@users.noreply.github.com>
2023-05-12 18:09:30 -0500

    agregando country a handlres y controller by:xavier

--- api/src/controllers/CompaniesController/CompanyControllerPost.js
+++ api/src/controllers/CompaniesController/CompanyControllerPost.js
@@ -1,7 +1,7 @@
 const { Company } = require('../../db');
 
-const postCompany = async (name, business_name, ruc, cuit, email, password, VacantId) => {
-  const newCompany = await Company.create({name, business_name, ruc, cuit, email, password, VacantId})
+const postCompany = async (name, business_name, ruc, cuit, country, email, password, VacantId) => {
+  const newCompany = await Company.create({name, business_name, ruc, cuit, country, email, password, VacantId})
   return newCompany
 };
 
