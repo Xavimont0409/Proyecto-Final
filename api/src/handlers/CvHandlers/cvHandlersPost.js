265eb641bf2857f0f42f6a64c398634abd3c33d0
Xavimont0409 <111645686+Xaviermon@users.noreply.github.com>
2023-05-13 15:47:18 -0500

    modificacion del modelos cv by:xavier

--- api/src/handlers/CvHandlers/cvHandlersPost.js
+++ api/src/handlers/CvHandlers/cvHandlersPost.js
@@ -1,15 +1,49 @@
-const errorUser = require('../../helpers/errors')
-const { createCv } = require('../../controllers/CvController/CvControllerPost')
+const errorUser = require("../../helpers/errors");
+const { createCv } = require("../../controllers/CvController/CvControllerPost");
 
-const cvHandlerPost = async(req, res) =>{
-    const { dni, name, lastName, address, photo, profession, github, linkedin, work_experience, personal_description, education, ApplicantId } = req.body
-    try {
-        res.status(200).json(await createCv(dni, name, lastName, address, photo, profession, github, linkedin, work_experience, personal_description, education, ApplicantId))
-    } catch (error) {
-        errorUser(error, res);
-    }
-}
+const cvHandlerPost = async (req, res) => {
+  const {
+    dni,
+    phone,
+    address,
+    photo,
+    profession,
+    linkedin,
+    personal_description,
+    skill,
+    country,
+    educational_institution,
+    state,
+    initial_date,
+    finish_date,
+    ApplicantId,
+  } = req.body;
+  try {
+    res
+      .status(200)
+      .json(
+        await createCv(
+          dni,
+          phone,
+          address,
+          photo,
+          profession,
+          linkedin,
+          personal_description,
+          skill,
+          country,
+          educational_institution,
+          state,
+          initial_date,
+          finish_date,
+          ApplicantId
+        )
+      );
+  } catch (error) {
+    errorUser(error, res);
+  }
+};
 
-module.exports={
-    cvHandlerPost,
-}
+module.exports = {
+  cvHandlerPost,
+};
