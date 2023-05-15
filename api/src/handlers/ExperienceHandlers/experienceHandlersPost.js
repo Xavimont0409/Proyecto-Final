c0533f5710abcaf564d44c3e3aa7c67b5904c279
lean0mar <lm.martinez.leandro@gmail.com>
2023-05-15 12:28:35 -0300

    Agregue unos cambios

--- api/src/handlers/ExperienceHandlers/experienceHandlersPost.js
+++ api/src/handlers/ExperienceHandlers/experienceHandlersPost.js
@@ -2,7 +2,7 @@ const errorUser = require("../../helpers/errors");
 const { createExperience } = require('../../controllers/ExperienceController/experienceControllerPost');
 
 const experienceHandlersPost = async(req, res) =>{
-    const { company, charge, experience_level, location, start_date, end_date, still_working, CvId } = req.body
+    const { company, charge, experience_level, location, start_date, end_date, still_working, CvId } = req.body;
     try {
         res.status(200).json(await createExperience(company, charge, experience_level, location, start_date, end_date, still_working, CvId));
     } catch (error) {
