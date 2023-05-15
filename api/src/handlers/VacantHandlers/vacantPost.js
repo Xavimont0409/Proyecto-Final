742ce4130924489fc913ae21c34bf46f85f63767
lean0mar <lm.martinez.leandro@gmail.com>
2023-05-15 12:29:17 -0300

    Agregue unos cambios

--- api/src/handlers/VacantHandlers/vacantPost.js
+++ api/src/handlers/VacantHandlers/vacantPost.js
@@ -1,8 +1,8 @@
-const errorUser = require('../../helpers/errors')
-const { postVacant } = require('../../controllers/VacantController/VacantPost')
+const errorUser = require('../../helpers/errors');
+const { postVacant } = require('../../controllers/VacantController/VacantPost');
 
 const vacantHandlerPost = async(req, res) =>{
-    const { title, description, CompanyId,WorkdayId, WorkMethodId, SeniorityId, creation_date } = req.body
+    const { title, description, CompanyId,WorkdayId, WorkMethodId, SeniorityId, creation_date } = req.body;
     try {
         res.status(200).json(await postVacant(title, description, CompanyId, WorkdayId, WorkMethodId, SeniorityId, creation_date ))
     } catch (error) {
