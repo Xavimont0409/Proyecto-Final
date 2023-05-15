ea2861cec4c61bcf394a404a5164933ca70105dc
JuanCruz <juancruzgenovese22@gmail.com>
2023-05-15 12:46:05 -0300

    ningun cambio

--- client/src/components/NavBar/NavBarUnlog.jsx
+++ client/src/components/NavBar/NavBarUnlog.jsx
@@ -3,7 +3,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-
     return (
         <>
             <Navbar bg="light" expand="sm">
