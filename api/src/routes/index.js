1d432c502b7ca301e27e28a8c74f6795eb24bdab
Joel Dupraz <joel_dupraz@hotmail.com>
2023-05-16 16:41:49 -0300

    Arreglo de index de route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,12 +5,10 @@ const applicantRouter = require('./ApplicantRouter');
 const cvRouter = require('./CvRouter');
 const operationRouter = require('./OperationRouter');
 const experienceRouter = require('./ExperienceRouter');
-<<<<<<<<< Temporary merge branch 1
 const getEmailRouter = require('./getEmailRouter')
-=========
 const stateRouter = require('./stateRouter')
 const productRouter = require('./productRouter')
->>>>>>>>> Temporary merge branch 2
+
 
 const router = Router();
 
@@ -20,11 +18,8 @@ router.use("/company", companysRouter);
 router.use("/createCv", cvRouter);
 router.use("/operation", operationRouter);
 router.use("/experience", experienceRouter);
-<<<<<<<<< Temporary merge branch 1
 router.use("/email", getEmailRouter)
-=========
 router.use("/state", stateRouter);
 router.use("/product", productRouter);
->>>>>>>>> Temporary merge branch 2
 
 module.exports = router;
