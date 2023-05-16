ee9619f5e287a8cbda04ff72839a1ca14d477ff2
Xavimont0409 <111645686+Xaviermon@users.noreply.github.com>
2023-05-16 10:37:00 -0500

    agregando modelos para validacion de login by:xavier

--- api/src/controllers/ApplicantController/ApplicantControllerGet.js
+++ api/src/controllers/ApplicantController/ApplicantControllerGet.js
@@ -1,13 +1,19 @@
-const { Applicant, Cv } = require('../../db')
+const { Applicant, Cv, State } = require("../../db");
 
-const allApplicant = async() =>{
-  return await Applicant.findAll({include: {model: Cv}});
-}
-const allApplicantId = async(id) =>{
-  return await Applicant.findAll({include: {model: Cv}, where: {id: id}});
-}
+const allApplicant = async () => {
+  return await Applicant.findAll({ include: [
+    { model: Cv },
+    { model: State }
+  ] });
+};
+const allApplicantId = async (id) => {
+  return await Applicant.findAll({
+    include: [{ model: Cv }, { model: State }],
+    where: { id: id },
+  });
+};
 
 module.exports = {
   allApplicant,
-  allApplicantId
-}
+  allApplicantId,
+};
