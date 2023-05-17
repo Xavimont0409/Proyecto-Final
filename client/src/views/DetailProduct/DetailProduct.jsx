bbc64d525083897f50bd02d4bf110005f5c97df7
lean0mar <lm.martinez.leandro@gmail.com>
2023-05-17 15:36:22 -0300

    Cambios para el detail de product

--- client/src/views/DetailProduct/DetailProduct.jsx
+++ client/src/views/DetailProduct/DetailProduct.jsx
@@ -1,8 +1,13 @@
 import style from './DetailProduct.module.css';
 import NavBar from '../../components/NavBar/NavBarLog';
 import Footer from '../../components/Footer/Footer';
+import { useSelector } from 'react-redux';
+
 
 const DetailProduct = () => {
+
+  const product = useSelector((state) => state.product)
+
 return (
     <div>
         <NavBar className={style.nav}/>
@@ -11,9 +16,9 @@ return (
         <div className={style.box}>
               <div className={style.containerDetail}>
              <div className={style.divContenedorInfo}>
-               <h2 className={style.nameCard}>Name: {/* {product.name} */}</h2>
-               <p className={style.infoCard}>Detalle: {/* {product.details} */}</p>
-               <p className={style.infoCard}>Price: {/* {product.pice} */}</p>
+               <h2 className={style.nameCard}>Name: {product.name}</h2>
+               <p className={style.infoCard}>Detalle: {product.details}</p>
+               <p className={style.infoCard}>Price: {product.price}</p>
              </div>
              </div>
            </div>
