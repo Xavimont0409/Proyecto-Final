0f029e2b60e77d3ab70827d2608a1d58c2e5a3cf
JuanCruz <juancruzgenovese22@gmail.com>
2023-05-16 23:08:39 -0300

    funcionalidad nueva: rutas protegidas listas paras conectarlas con el estado global de usuarios

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
 import Error404 from './components/Error404/Error404';
 import { useState } from 'react';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 
 
 axios.defaults.baseURL = 'http://localhost:3001'
@@ -26,22 +27,25 @@ function App() {
 
   return (
     <div className="App">
+      { user ? (<button onClick={logout}>Logout</button>) : (<button onClick={login}>Login</button>)}
       <Routes>
+        <Route element={<ProtectedRoute user={user}/>}>
+          <Route path="/empleoDetail/:detailId" element={<EmpleoDetail />} />
+          <Route path="/empresa" element={<LandingEmpresa />}></Route>
+          <Route path="/registro-cv" element={<FormCv />}></Route>
+          <Route path="/registro-empresa" element={<FormEmpresa />}></Route>
+          <Route path="/registro-vacante" element={<FormVacante />}></Route>
+          <Route path='/profiles' element={<Profiles/>} />
+          <Route path='/applicant' element={<Applicant/>} />
+          <Route path='/profiles-company' element={<ProfilesCompany/>} />
+          <Route path="/MiPerfil" element={<MiPerfil/>}></Route>
+        </Route>
         <Route exact path="/" element={<Landing />}></Route>
-        <Route path="/empleoDetail/:detailId" element={<EmpleoDetail />} />
         <Route path="/empleos" element={<Empleos/>}></Route>
-        <Route path="/empresa" element={<LandingEmpresa />}></Route>
         <Route path="/iniciarSesion" element={<IniciarSesion />}></Route>
         <Route path="/registro" element={<Registro />}></Route>
         <Route path="/registroini-empresa" element={<FormRegisterEmpresa />}></Route>
         <Route path="/registro-usuario" element={<FormRegistroUsuario />}></Route>
-        <Route path="/registro-cv" element={<FormCv />}></Route>
-        <Route path="/registro-empresa" element={<FormEmpresa />}></Route>
-        <Route path="/registro-vacante" element={<FormVacante />}></Route>
-        <Route path='/profiles' element={<Profiles/>} />
-        <Route path='/applicant' element={<Applicant/>} />
-        <Route path='/profiles-company' element={<ProfilesCompany/>} />
-        <Route path="/MiPerfil" element={<MiPerfil/>}></Route>
         <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
--- client/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
+import { Navigate, Outlet } from "react-router-dom";
 
+const ProtectedRoute = ({ user, redirectTo="/iniciarSesion"}) => {
+    if( !user ) {
+        return <Navigate to={redirectTo}/>
+    }
 
-const ProtectedRoute = () => {
-    return (
-        <div>
-            Ruta protegida
-        </div>
-    )
+    return <Outlet/>
 }
 
 export default ProtectedRoute;
