644d73a6d154260d3b4583213de478176670df56
Xavimont0409 <111645686+Xaviermon@users.noreply.github.com>
2023-05-18 15:29:23 -0500

    router relation

--- api/src/controllers/VacantController/VacantPost.js
+++ api/src/controllers/VacantController/VacantPost.js
@@ -1,10 +1,17 @@
-const { Vacant, Workday } = require('../../db');
+const { Vacant, Applicant } = require('../../db');
 
 const postVacant = async(title, description, CompanyId, WorkdayId, WorkMethodId, SeniorityId, creation_date ) =>{
     const newVacant = await Vacant.create({title, description, CompanyId, WorkdayId, WorkMethodId, SeniorityId, creation_date });
     return newVacant;
 }
+const postVacantRelation = async(VacantId, ApplicantId)=>{
+    const findVacant = await Vacant.findAll({where: {id :VacantId}})
+    const findApplicant	= await Applicant.findAll({where: {id : ApplicantId}})
+    await findVacant.setApplicant(findApplicant)
+    return findVacant
+}
 
 module.exports={
-    postVacant
+    postVacant,
+    postVacantRelation
 }
--- api/src/routes/VacantRouter.js
+++ api/src/routes/VacantRouter.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const { vacantHandlerGetId, vacantHandlerGet } = require('../handlers/VacantHandlers/vacantGet');
-const { vacantHandlerPost } = require('../handlers/VacantHandlers/vacantPost');
+const { vacantHandlerPost, vacantHandlerPostRelation } = require('../handlers/VacantHandlers/vacantPost');
 
 const vacantRouter = Router();
 
-vacantRouter.get('/', vacantHandlerGet); //!Todos los empleos y empleo por body input
+vacantRouter.get('/', vacantHandlerGet);
 vacantRouter.get('/:id', vacantHandlerGetId);
 vacantRouter.post('/', vacantHandlerPost);
+vacantRouter.post('/relation', vacantHandlerPostRelation)
 
 
 module.exports = vacantRouter;
