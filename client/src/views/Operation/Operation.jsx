3ab10dfae3fa327a8d71557a0b031aaf5975dd7d
lean0mar <lm.martinez.leandro@gmail.com>
2023-05-19 15:58:32 -0300

    Hice algo

--- client/src/views/Operation/Operation.jsx
+++ client/src/views/Operation/Operation.jsx
@@ -1,7 +1,7 @@
 import style from './Operation.module.css';
 import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
 import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import Loading from '../../components/Loading/Loading';
 import {Footer, NavBar} from '../../components/';
 
@@ -22,29 +22,25 @@ const Operation = () => {
                     <form>
                         <div className={style.Texto}>
                             <div>
-                                <label htmlFor="name">Detail:</label>
+                                <label htmlFor="name">Plan:</label>
                                 <input name="name" value={info.detail} type="text" disabled />
                             </div>
                             <div>
-                                <label htmlFor="name">Details:</label>
+                                <label htmlFor="name">Detalle:</label>
                                 <input name="name" value={info.details} type="text" disabled />
                             </div>
                             <div>
-                                <label htmlFor="name">Cost:</label>
+                                <label htmlFor="name">Precio:</label>
                                 <input name="name" value={info.cost} type="text" disabled />
                             </div>
                             <div>
-                                <label htmlFor="name">PayMethodId:</label>
+                                <label htmlFor="name">Metodo de Pago:</label>
                                 <input name="name" value={info.PayMethodId} type="text" disabled />
                             </div>
                         </div>
                     </form>
                     <div className={style.botonpago}>
-                        <button className={style.btnPagar}>
-                            Pagar
                             <Wallet className={style.btnPagar} initialization={{ preferenceId: preferenceId }} />
-                        </button>
-
                     </div>
                 </div>
             ) : (
