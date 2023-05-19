a308741b948cfe753963539f7c8b242ae9f24b94
Victor <henry079@gmail.com>
2023-05-19 11:00:38 -0500

    se deja en true la proiedad allowNull de fecha final de estudio y experiencia

--- api/src/models/CV.js
+++ api/src/models/CV.js
@@ -57,7 +57,7 @@ module.exports = (sequelize) => {
           },
           finish_date: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: true
           }
     });
 };
