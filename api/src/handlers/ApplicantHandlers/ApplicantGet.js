4d28c104894b4f302eb44c0d28caad72aee6a5c4
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-05-22 20:04:14 -0300

    Update ApplicantGet.js

--- api/src/handlers/ApplicantHandlers/ApplicantGet.js
+++ api/src/handlers/ApplicantHandlers/ApplicantGet.js
@@ -6,7 +6,7 @@ const sortNorder = require('../../controllers/utils/sort&Order');
 
 const applicantHandlerGet = async (req, res) => {
 
-    const { _sort, _order, _end, _start, name } = req.query;
+    const { _sort, _order, name } = req.query;
 
     try {
         const response = await allApplicant();
@@ -15,7 +15,10 @@ const applicantHandlerGet = async (req, res) => {
             finalResult = filterData(name, finalResult); //aca se filtran los datos(si es que hay un valor de filtrado)
         }
         //si no habia un valor por el cual filtrar los resultados simplemente se ordenan los datos recibidos y se envian como respuestaFinal
-        sortNorder(_sort, _order, finalResult); //aca se ordenan en relacion a que dato quieren ser ordenados
+        if(_sort && _order){
+            sortNorder(_sort, _order, finalResult); //aca se ordenan en relacion a que dato quieren ser ordenados
+        }
+        //si no hay solicitud de orden por query, entonces simplemente se avanza al final y se devuelve los datos tal cual llegaron
         setXTotalCount(res, finalResult.length); //aca se asigna un header X-Total-Count requerido por las tablas de react-admin
         res.status(200).json(finalResult);
     } catch (error) {
