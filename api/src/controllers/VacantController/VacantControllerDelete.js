70d5a5406721c95f3009156eb1ead0a17acff9b3
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-05-23 15:16:35 -0300

    Update VacantControllerDelete.js

--- api/src/controllers/VacantController/VacantControllerDelete.js
+++ api/src/controllers/VacantController/VacantControllerDelete.js
@@ -4,6 +4,20 @@ const vacantDelete = async (id, body) => {
   let response = {};
   await Vacant.update({status: false}, {
     where: { id: id },
+    include: [{model:Applicant},
+      {
+        model: Workday,
+        attributes: ["name"],
+      },
+      {
+        model: WorkMethod,
+        attributes: ["name"]
+      },
+      {
+        model: Seniority,
+        attributes: ["name"]
+      }
+    ],
   }).then(num => {
     if (num == 1) response = body;
   });
