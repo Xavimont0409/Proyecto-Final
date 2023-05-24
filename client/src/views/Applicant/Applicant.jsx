f4423ab2aba2f520b5b8156d1b11a6c4cdc6dfe9
lean0mar <lm.martinez.leandro@gmail.com>
2023-05-24 15:17:04 -0300

    Applicant hecho de la manera pedida

--- client/src/views/Applicant/Applicant.jsx
+++ client/src/views/Applicant/Applicant.jsx
@@ -1,128 +1,118 @@
-import NavBar from '../../components/NavBar/NavBar';
-import React from 'react';
+import NavBar from "../../components/NavBar/NavBar";
+import React from "react";
+import { useEffect, useState } from "react";
 import styles from "./Applicant.module.css";
-import { Carousel, Card, Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
-import img1 from "../../assets/img/empleo1.jpg";
-import img2 from "../../assets/img/empleo2.jpg";
-import img3 from "../../assets/img/empleo3.jpg";
 import CardPlan from "../../components/CardPlan/CardPlan";
+import { useAuth0 } from "@auth0/auth0-react";
+import img from "../../assets/img/empleo1.jpg";
 
 export default function Applicant({ setCurrentUserStore }) {
-  return (
-    <>
-    
-      <NavBar setCurrentUserStore={setCurrentUserStore}/>
+	const [greeting, setGreeting] = useState("");
+	const { isAuthenticated, user } = useAuth0();
 
-      <div className={styles.container}>
-      <Carousel>
-        {/* Slide 1 */}
-        <Carousel.Item>
-          <img
-            className={styles.slide}
-            src={img1}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>Acme Technologies</h3>
-            <p>Click para más detalle</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        {/* Slide 2 */}
-        <Carousel.Item>
-          <img
-            className={styles.slide}
-            src={img2}
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Green Energy Solutions</h3>
-            <p>Click para más detalle</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        {/* Slide 3 */}
-        <Carousel.Item>
-          <img
-            className={styles.slide}
-            src={img3}
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Stellar Consulting</h3>
-            <p>Click para más detalle</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-        <Button variant="secondary" className="mt-3">
-          <Link to="/empleos" style={{ color: "white", textDecoration: "none" }}>
-            Ver más
-          </Link>
-        </Button>
-      <div className={styles.SuggestedJob}>
-        <h4>Sugerencias de vacantes</h4>
-        <div className={styles.Vacancies}>
-          <Card className={styles.job}>
-            <Card.Body>
-              <Card.Title>Desarrollador de software</Card.Title>
-              <Card.Text>Responsable de diseñar, desarrollar y mantener soluciones de software innovadoras para impulsar la transformación digital de los clientes.</Card.Text>
-              <Button variant="primary">Conoce mas</Button>
-            </Card.Body>
-          </Card>
-          <Card className={styles.job}>
-            <Card.Body>
-              <Card.Title>Gerente de proyectos</Card.Title>
-              <Card.Text>Responsable de planificar, ejecutar y supervisar proyectos para garantizar su éxito y cumplimiento de los objetivos establecidos.</Card.Text>
-              <Button variant="primary">Conoce mas</Button>
-            </Card.Body>
-          </Card>
-          <Card className={styles.job}>
-            <Card.Body>
-              <Card.Title>Técnico de instalaciones solares</Card.Title>
-              <Card.Text>Encargado de instalar y mantener sistemas de energía solar en residencias o empresas.</Card.Text>
-              <Button variant="primary">Conoce mas</Button>
-            </Card.Body>
-          </Card>
-            <Card className={styles.job}>
-          <Card.Body>
-              <Card.Title>Analista de impacto ambiental</Card.Title>
-              <Card.Text>Responsable de evaluar el impacto ambiental de proyectos y proponer medidas para minimizar su huella ambiental.</Card.Text>
-              <Button variant="primary">Conoce mas</Button>
-            </Card.Body>
-          </Card>
-        <Card className={styles.job}>
-            <Card.Body>
-              <Card.Title>Consultor estratégico</Card.Title>
-              <Card.Text>Encargado de brindar asesoramiento estratégico a las empresas clientes para mejorar su rendimiento y alcanzar sus objetivos de negocio.</Card.Text>
-              <Button variant="primary">Conoce mas</Button>
-            </Card.Body>
-          </Card>
-        <Card className={styles.job}>
-            <Card.Body>
-              <Card.Title>Analista de datos</Card.Title>
-              <Card.Text>Responsable de recopilar, analizar y presentar datos relevantes para ayudar en la toma de decisiones informadas.</Card.Text>
-              <Button variant="primary">Conoce mas</Button>
-            </Card.Body>
-          </Card>
-          {/* Add more job cards here */}
-        </div>
-        <div className={styles.containerCardPlan}>
-          <CardPlan 
-                        tittle='Plan Super Destacado'
-                        text ='Aumenta la visibilidad de tu perfil profesional y recibe aviso de nuevas propuestas '
-                        price = '5'
-                        id = '4' />
-        </div>
-        <Button variant="secondary" className="mt-3">
-          <Link to="/empleos" style={{ color: "white", textDecoration: "none" }}>
-            Todas las vacantes
-          </Link>
-        </Button>
-        <br />
-        <br />
+	useEffect(() => {
+		if (isAuthenticated && user) {
+			const name = user.name;
+			setGreeting(`Hola, ${name}!`);
+		}
+	}, [isAuthenticated, user]);
+	return (
+		<>
+    <div className={styles.container}>
+      <div className={styles.containerComponents}>
+        <NavBar setCurrentUserStore={setCurrentUserStore} ></NavBar>
+      </div>
 
+      <div className={styles.containerPrincipal}>
+        <div className={styles.saludo}>
+          <h1 className={styles.titulo}>{greeting}</h1>
+        </div>
+        <div>
+          <div className={styles.bienvenida}>
+            <h3 className={styles.info}>Te deseamos una bienvenida a JobPortalX</h3>
+          </div>
+          <div className={styles.pregunta}>
+            <h4 className={styles.info}>¿Qué quieres hacer?</h4>
+          </div>
+        </div>
+        <div className={styles.containerButtons}>
+						<Link to='/profiles'>
+							<button className={styles.Button}>
+								Mis postulaciones
+							</button>
+						</Link>
+						<Link to='/registro-cv'>
+							<button className={styles.Button}>Cv</button>
+						</Link>
+						<Link to='/registro-experiencia'>
+							<button className={styles.Button}>
+								Experience
+							</button>
+						</Link>
+						<Link to='/empleos'>
+							<button className={styles.Button}>Vacantes</button>
+						</Link>
+						<Link to='/Miperfil'>
+							<button className={styles.Button}>Mi perfil</button>
+						</Link>
+						<Link to='/profiles-company'>
+							<button className={styles.Button}>
+								Perfiles Empresa
+							</button>
+						</Link>
+						<Link to='/MiEducacion'>
+							<button className={styles.Button}>
+								Mi educacion
+							</button>
+						</Link>
       </div>
       </div>
-    </>
-  );
+    </div>
+ <div className={styles.contenedor}>
+				<div className={styles.prueba}>
+					<div className={styles.leftSection}>
+						<h1>Este es el Home de Applicant</h1>
+					</div>
+
+					<div className={styles.rightSection}>
+						<img src={img} alt='Servidor en Mantenimiento' />
+					</div>
+				</div>
+
+				<div className={styles.prueba}>
+					<div className={styles.leftSection}>
+						<img src={img} alt='Servidor en Mantenimiento' />
+					</div>
+
+					<div className={styles.middleSection}>
+						<h1>Joel</h1>
+					</div>
+
+					<div className={styles.rightSection}>
+						<img src={img} alt='Servidor en Mantenimiento' />
+					</div>
+				</div>
+
+				<div className={styles.prueba}>
+					<div className={styles.leftSection}>
+						<div className={styles.planes}>
+							<CardPlan
+								tittle='Plan Super Destacado'
+								text='Aumenta la visibilidad de tu perfil profesional y recibe aviso de nuevas propuestas '
+								price='5'
+								id='4'
+							/>
+						</div>
+					</div>
+
+					<div className={styles.rightSection}>
+						<h1>Actualiza tú plan!</h1>
+					</div>
+				</div>
+				</div>
+				<br />
+				<br />
+		</>
+	);
 }
-
