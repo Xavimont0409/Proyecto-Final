805bc7c4526b8972971a04d17ad4e7105f0b02a9
lean0mar <lm.martinez.leandro@gmail.com>
2023-05-25 20:24:26 -0300

    S

--- api/src/models/Formation.js
+++ api/src/models/Formation.js
@@ -32,11 +32,11 @@ module.exports = (sequelize) => {
             allowNull: false
         },
         start_date: {
-            type: DataTypes.STRING,
+            type: DataTypes.DATEONLY,
             allowNull: false,
         },
         end_date: {
-            type: DataTypes.STRING,
+            type: DataTypes.DATEONLY,
 
         },
     },
