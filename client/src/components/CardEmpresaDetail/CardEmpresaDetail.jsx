67800b23639417e9f0ccc617d32ccd426dd2e480
Andres Felipe Rojas Perez <you@example.com>
2023-05-29 17:43:10 -0500

    arreglo_vista_empleoDetail_y_logica_del_logo_en_cards_de_empleos

--- client/src/components/CardEmpresaDetail/CardEmpresaDetail.jsx
+++ client/src/components/CardEmpresaDetail/CardEmpresaDetail.jsx
@@ -23,20 +23,21 @@ const CardEmpresaDetail = ({ companyId }) => {
   const company = useSelector(state => state.CompanyDetail);
 
   return (
-    <Card style={{ width: '100%' }}>
+    <Card className={style.mainContainer} style={{ width: '100%' }}>
       <div>
-        <Card.Img className={style.logo} variant="top" src="https://th.bing.com/th/id/R.a30e39eb5d4a9e55f49c052732ad4504?rik=LwN38PDNxREmFw&riu=http%3a%2f%2fwww.cleankontor.de%2fwp-content%2fuploads%2f2015%2f02%2fGebaeudeservice-Gebaeudereinigung-Hamburg-300x300.png&ehk=gtecgngKr%2fcTb6%2b2AN9BhhSqvB8pTESppbZbM0LMDIg%3d&risl=&pid=ImgRaw&r=0" />
+        <Card.Img className={style.logo} variant="top" src={company?.photo} />
       </div>
       <Card.Body>
         <Card.Title>{company?.business_name}</Card.Title>
       </Card.Body>
-      <ListGroup className="list-group-flush">
+      <ListGroup className={style.container2}>
         <ListGroup.Item>Razón Social: {company?.name}</ListGroup.Item>
         <ListGroup.Item><TfiEmail className={style.icon} />{company?.email}</ListGroup.Item>
         <ListGroup.Item>País: {company?.country}</ListGroup.Item>
         <ListGroup.Item>CUIT: {company?.cuit}</ListGroup.Item>
-        <ListGroup.Item>
-        <button className={styles.commentButton} onClick={toggleComments}>
+      </ListGroup>
+      <Card.Body>
+      <button className={styles.commentButton} onClick={toggleComments}>
             {showComments ? 'Ocultar comentarios' : 'Mostrar comentarios'}
           </button>
           {showStarsAndUser && (
@@ -44,8 +45,7 @@ const CardEmpresaDetail = ({ companyId }) => {
               {company && <StarReview starsData={company?.Stars} showComments={showComments} companyId={companyId}/>}
             </div>
           )}
-        </ListGroup.Item>
-      </ListGroup>
+      </Card.Body>
     </Card>
   );
 };
