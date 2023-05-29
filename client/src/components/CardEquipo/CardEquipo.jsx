7941f3f8c9a4747551118d01fd7887b3afa7d99f
lean0mar <lm.martinez.leandro@gmail.com>
2023-05-29 02:05:14 -0300

    Cambios luego de testeos

--- client/src/components/CardEquipo/CardEquipo.jsx
+++ client/src/components/CardEquipo/CardEquipo.jsx
@@ -6,63 +6,72 @@ import { Link } from 'react-router-dom';
 const CardsEquipo = () => {
 
     const arrayEquipo = [
-        {id:1, 
-        name: "Joel",
-        lastname: "Dupraz",
-        github: "https://github.com/JoelDUAR",
-        linkedin: "https://www.linkedin.com/in/joel-dupraz-ardiles/",
-        photo:"https://avatars.githubusercontent.com/u/85464395?v=4"
-        },
-        {id:2, 
-        name: "Leandro",
-        lastname: "Martinez",
-        github: "https://github.com/Lean0mar",
-        linkedin: "https://www.linkedin.com/in/lean0mar/",
-        photo:"https://avatars.githubusercontent.com/u/122496170?v=4"
-        },
-        {id:3, 
-        name: "Xavier",
-        lastname: "Montero",
-        github: "https://github.com/Xavimont0409",
-        linkedin: "https://www.linkedin.com/in/xavier-montero-b343b8231/",
-        photo:"https://avatars.githubusercontent.com/u/122754733?v=4"
-        },
-        { id:4,
-            name: "Augusto",
-        lastname: "Viggiano",
-        github: "https://github.com/AugusViggia",
-        linkedin: "https://www.linkedin.com/in/augusto-viggiano-195134213/",
-        photo:"https://avatars.githubusercontent.com/u/112136936?v=4"
-        },
-        {id:5, 
-        name: "Juan",
-        lastname: "Genovese",
-        github: "https://github.com/JuanGenovese",
-        linkedin: "https://www.linkedin.com/in/juancruzgenovese/",
-        photo:"https://avatars.githubusercontent.com/u/107717991?v=4"
-        },
-        { id:6,
-        name: "Andrés",
-        lastname: "Rojas Pérez",
-        github: "https://github.com/AndresFelipeRojasPerez",
-        linkedin: "https://www.linkedin.com/in/andr%C3%A9s-felipe-rojas-p%C3%A9rez-500491262/",
-        photo:"https://avatars.githubusercontent.com/u/109153596?v=4"
-        },
-        { id:7,
-        name: "Victor",
-        lastname: "Rua Montalvo",
-        github: "https://github.com/henryrua079",
-        linkedin: "https://www.linkedin.com/in/victor-rua/",
-        photo:"https://avatars.githubusercontent.com/u/110183477?v=4"
-        },
-        {id: 8,
-        name: "Gaspar",
-        lastname: "Almaraz",
-        github: "https://github.com/GasparAlmaraz",
-        linkedin: "https://www.linkedin.com/in/gaspar-almaraz-b12840209/",
-        photo:"https://avatars.githubusercontent.com/u/69637544?v=4"
-        },
-    ]
+		{
+			id: 1,
+			name: "Joel",
+			lastname: "Dupraz",
+			github: "https://github.com/JoelDUAR",
+			linkedin: "https://www.linkedin.com/in/joel-dupraz-ardiles/",
+			photo: "https://avatars.githubusercontent.com/u/85464395?v=4",
+		},
+		{
+			id: 2,
+			name: "Leandro",
+			lastname: "Martinez",
+			github: "https://github.com/Lean0mar",
+			linkedin: "https://www.linkedin.com/in/lean0mar/",
+			photo: "https://avatars.githubusercontent.com/u/122496170?v=4",
+		},
+		{
+			id: 3,
+			name: "Xavier",
+			lastname: "Montero",
+			github: "https://github.com/Xavimont0409",
+			linkedin: "https://www.linkedin.com/in/xavier-montero-b343b8231/",
+			photo: "https://avatars.githubusercontent.com/u/122754733?v=4",
+		},
+		{
+			id: 4,
+			name: "Augusto",
+			lastname: "Viggiano",
+			github: "https://github.com/AugusViggia",
+			linkedin: "https://www.linkedin.com/in/augusto-viggiano-195134213/",
+			photo: "https://media.licdn.com/dms/image/D4D35AQH8L5X2HcaMtQ/profile-framedphoto-shrink_400_400/0/1678809187643?e=1685944800&v=beta&t=tBNpPD96jHh_QxZDzrq5GV0WNtF8cHLSGHaqwczBsx4",
+		},
+		{
+			id: 5,
+			name: "Juan",
+			lastname: "Genovese",
+			github: "https://github.com/JuanGenovese",
+			linkedin: "https://www.linkedin.com/in/juancruzgenovese/",
+			photo: "https://media.licdn.com/dms/image/C4D03AQGJ7V3D1HlPkA/profile-displayphoto-shrink_400_400/0/1661969833585?e=1691020800&v=beta&t=8wnGtiH7Bv2ELMODecgLK1MLQQIOAT0qCH1DFZNYnoU",
+		},
+		{
+			id: 6,
+			name: "Andrés",
+			lastname: "Rojas Pérez",
+			github: "https://github.com/AndresFelipeRojasPerez",
+			linkedin:
+				"https://www.linkedin.com/in/andr%C3%A9s-felipe-rojas-p%C3%A9rez-500491262/",
+			photo: "https://avatars.githubusercontent.com/u/109153596?v=4",
+		},
+		{
+			id: 7,
+			name: "Victor",
+			lastname: "Rua Montalvo",
+			github: "https://github.com/henryrua079",
+			linkedin: "https://www.linkedin.com/in/victor-rua/",
+			photo: "https://avatars.githubusercontent.com/u/110183477?v=4",
+		},
+		{
+			id: 8,
+			name: "Gaspar",
+			lastname: "Almaraz",
+			github: "https://github.com/GasparAlmaraz",
+			linkedin: "https://www.linkedin.com/in/gaspar-almaraz-b12840209/",
+			photo: "https://media.licdn.com/dms/image/C4D03AQEwnnjkwBWnqA/profile-displayphoto-shrink_400_400/0/1649357851670?e=1691020800&v=beta&t=F1FpHIvEHqeozL0eNfPz62eGHlO0tmJzJ6e22Cx7e2w",
+		},
+	];
 
     return (
         <div className={style.containerPadre}>
