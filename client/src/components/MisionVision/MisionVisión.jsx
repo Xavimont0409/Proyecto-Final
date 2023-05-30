277d080388c480cf01af3a7e9124faa0f9a0ad75
Joel Dupraz <joel_dupraz@hotmail.com>
2023-05-30 04:08:46 -0300

    trabajo de landing nueva

--- client/src/components/MisionVision/MisionVisión.jsx
+++ client/src/components/MisionVision/MisionVisión.jsx
@@ -6,13 +6,13 @@ const MisionVisión = () => {
         <div className={style.container}>
             <div className={style.box}>
                 <div className={style.containerInfo}>
-                    <h2 className={style.title}>Nuestra Misión</h2>
+                    <h2 className={style.title}>Misión</h2>
                     <p className={style.text}>Aportar al mercado laboral, un portal de empleo que permita tener las mayores oportunidades, con las mejores actualizaciones para contribir a un futuro sustentable y socialmente responsable </p>
                 </div>
             </div>
             <div className={style.box}>
                 <div className={style.containerInfo}>
-                    <h2 className={style.title}>Nuestra Visión</h2>
+                    <h2 className={style.title}>Visión</h2>
                     <p className={style.text}>JobPortalX podrá posicionarse como un gigante de empleabilidad para empoderar a empresas y postulantes para que puedan crear los mejores vinculos laborales, constribuyendo a mejorar el mercado laboral</p>
                 </div> 
             </div>            
