6d82c76621da1ce0df54e9f67de673ca5624547d
Xavimont0409 <111645686+Xaviermon@users.noreply.github.com>
2023-05-30 13:45:12 -0500

    cambios login

--- client/src/views/NewRegistro/newResgistroApplicant.jsx
+++ client/src/views/NewRegistro/newResgistroApplicant.jsx
@@ -3,10 +3,14 @@ import GoogleLogin from "react-google-login";
 import { useEffect, useState } from "react";
 import FormRegistroUsuario from "../FormRegistroUsuario/FormRegistroUsuario";
 import style from "./NewRegistro.module.css";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link, Route, Routes } from "react-router-dom";
 
-const NewRegistroApplicant = ({ setCurrentUserStore2, setValidateState, setCurrentUserStore }) => {
-  const navigate = useNavigate()
+const NewRegistroApplicant = ({
+  setCurrentUserStore2,
+  setValidateState,
+  setCurrentUserStore,
+}) => {
+  const navigate = useNavigate();
   const clientID =
     "970075390910-oaut1poeo5kbmti73j5fm8t3mrpi8jk7.apps.googleusercontent.com";
   const [Applicant, setApplicant] = useState({
@@ -33,7 +37,7 @@ const NewRegistroApplicant = ({ setCurrentUserStore2, setValidateState, setCurre
       email: newUserApplicant.email,
       nombre: newUserApplicant.givenName,
       apellido: newUserApplicant.familyName,
-      contraseña: newUserApplicant.googleId
+      contraseña: newUserApplicant.googleId,
     });
   };
   const onFailure = () => {
@@ -45,28 +49,40 @@ const NewRegistroApplicant = ({ setCurrentUserStore2, setValidateState, setCurre
       <div className={style.containerData}>
         <h1 className={style.title}>Registro de Applicante</h1>
         <div>
-        <button className={style.button} onClick={()=> navigate('/registro-usuario')} >Registrarte con otro correo</button>
-        <GoogleLogin
-          clientId={clientID}
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          cookiePolicy={"single_host_policy"}
-        />
-      {
-        Applicant.nombre.length > 1 ? <FormRegistroUsuario setCurrentUserStore2={setCurrentUserStore2} Applicant={Applicant} setCurrentUserStore={setCurrentUserStore} setValidateState={setValidateState}/> : <div></div>
-      }
-
+          <button
+            className={style.button}
+            onClick={() => navigate("/registro-usuario")}
+          >
+            Registrarte con otro correo
+          </button>
+          <GoogleLogin
+            clientId={clientID}
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            cookiePolicy={"single_host_policy"}
+          />
+          <div className={style.containerRutaAlternativa}>
+            <span className={style.textoAlternativa}>
+              ¿Quieres iniciar sesión?
+            </span>
+            <Link to="/loginApplicant">
+              <span className={style.textoLink}>Inicio de Sesión</span>
+            </Link>
+          </div>
         </div>
-       
       </div>
-      <div className={style.containerRutaAlternativa}>
-        <span className={style.textoAlternativa}>
-        ¿Quieres iniciar sesión?
-        </span>
-        <Link to='/loginApplicant'><span className={style.textoLink}>Inicio de Sesión</span></Link>
-      </div>
+      {Applicant.nombre.length > 1 ? (
+        <FormRegistroUsuario
+          setCurrentUserStore2={setCurrentUserStore2}
+          Applicant={Applicant}
+          setCurrentUserStore={setCurrentUserStore}
+          setValidateState={setValidateState}
+        />
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
 
-export default NewRegistroApplicant
+export default NewRegistroApplicant;
