e3789cfd613e6ce7431b576afefd43d28e709449
Victor <henry079@gmail.com>
2023-05-31 02:46:35 -0500

    Se realizan cambios en mi perfil

--- client/src/views/FormVacante/FormVacante.jsx
+++ client/src/views/FormVacante/FormVacante.jsx
@@ -1,14 +1,7 @@
 import Form from "react-bootstrap/Form";
 import style from "./FormVacante.module.css";
 import { useState } from "react";
-import {
-  FormGroup,
-  FormLabel,
-  FormSelect,
-  FormControl,
-  Row,
-  Col,
-} from "react-bootstrap";
+import { FormGroup, FormLabel, FormSelect, FormControl, Row, Col, } from "react-bootstrap";
 import NavBar from "../../components/NavBarLog/NavBarLog";
 import ButtonGeneral from "../../components/Button/ButtonGeneral";
 import { useDispatch } from "react-redux";
@@ -41,7 +34,11 @@ export default function FormVacante({ setValidateState, setCurrentUserStore2 }) 
   const handleInputChange = (event) => {
     const property = event.target.name;
     const value = event.target.value;
-    setNewVacant({ ...newVacant, [property]: value });
+    if (property === 'title') {
+      setNewVacant({ ...newVacant, title: value.toUpperCase() })
+    } else {
+      setNewVacant({ ...newVacant, [property]: value });
+    }
   };
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -60,116 +57,115 @@ export default function FormVacante({ setValidateState, setCurrentUserStore2 }) 
   return (
     <div className={style.mainContainer}>
       <div className={style.containerFirst}>{/* no quitar */}</div>
-			<div className={style.containerSecond}>
-      <NavBar
-        setValidateState={setValidateState}
-        setCurrentUserStore2={setCurrentUserStore2}
-      ></NavBar>
-      <div className={style.title}>
-      <h2>Crear nueva vacante</h2>
-      </div>
-      <Form
-        validated={!validated}
-        className={style.Form}
-        onSubmit={handleSubmit}
-      >
-        <Form.Group as={Col} md="12" className="mb-3">
-          <FormLabel>Titulo de la vacante</FormLabel>
-          <FormControl
-            name="title"
-            placeholder="Titulo de tu nuvea vacante"
-            value={newVacant.title}
-            type="text"
-            onChange={handleInputChange}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Rellena este campo
-          </Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group as={Col} md="12" className="mb-3">
-          <Form.Label>Descripcion de la vacante</Form.Label>
-          <Form.Control
-            name="description"
-            value={newVacant.description}
-            placeholder="Realiza una descripción detallada de tu vacante"
-            as="textarea"
-            rows={5}
-            onChange={handleInputChange}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Rellena este campo
-          </Form.Control.Feedback>
-        </Form.Group>
-
-        <Row>
-          <FormGroup as={Col} md="4">
-            <FormLabel className="me-2">Modalidad</FormLabel>
-            <FormSelect
-              name="WorkMethodId"
-              value={newVacant.WorkMethodId}
+      <div className={style.containerSecond}>
+        <NavBar
+          setValidateState={setValidateState}
+          setCurrentUserStore2={setCurrentUserStore2}
+        ></NavBar>
+        <div className={style.title}>
+          <h2>Crear nueva vacante</h2>
+        </div>
+        <Form
+          validated={!validated}
+          className={style.Form}
+          onSubmit={handleSubmit}
+        >
+          <Form.Group as={Col} md="12" className="mb-3">
+            <FormLabel>Titulo de la vacante</FormLabel>
+            <FormControl
+              name="title"
+              placeholder="Titulo de tu nuvea vacante"
+              value={newVacant.title.toUpperCase()}
+              type="text"
               onChange={handleInputChange}
               required
-            >
-              <option disabled></option>
-              <option value={1}>Presencial</option>
-              <option value={2}>Hibrido</option>
-              <option value={3}>Remoto</option>
-            </FormSelect>
+            />
             <Form.Control.Feedback type="invalid">
-              Selecciona una opcion
+              Rellena este campo
             </Form.Control.Feedback>
-          </FormGroup>
+          </Form.Group>
 
-          <FormGroup as={Col} md="4">
-            <FormLabel className="me-2">Jornada</FormLabel>
-            <FormSelect
-              name="WorkdayId"
-              value={newVacant.WorkdayId}
+          <Form.Group as={Col} md="12" className="mb-3">
+            <Form.Label>Descripcion de la vacante</Form.Label>
+            <Form.Control
+              name="description"
+              value={newVacant.description}
+              placeholder="Realiza una descripción detallada de tu vacante"
+              as="textarea"
+              rows={5}
               onChange={handleInputChange}
               required
-            >
-              <option disabled></option>
-              <option value={1}>Tiempo completo</option>
-              <option value={2}>Medio tiempo</option>
-              <option value={3}>Otro</option>
-            </FormSelect>
+            />
             <Form.Control.Feedback type="invalid">
-              Selecciona una opcion
+              Rellena este campo
             </Form.Control.Feedback>
-          </FormGroup>
+          </Form.Group>
 
-          <FormGroup as={Col} md="4">
-            <FormLabel className="me-2">Experiencia</FormLabel>
-            <FormSelect
-              name="SeniorityId"
-              value={newVacant.SeniorityId}
-              onChange={handleInputChange}
-              required
-            >
-              <option disabled></option>
-              <option value={0}>Sin Experiencia</option>
-              <option value={4}>Trainee</option>
-              <option value={3}>Junior</option>
-              <option value={2}>Semi-senior</option>
-              <option value={1}>Senior</option>
-            </FormSelect>
-            <Form.Control.Feedback type="invalid">
-              Selecciona una opcion
-            </Form.Control.Feedback>
-          </FormGroup>
-        </Row>
-      </Form>
+          <Row>
+            <FormGroup as={Col} md="4">
+              <FormLabel className="me-2">Modalidad</FormLabel>
+              <FormSelect
+                name="WorkMethodId"
+                value={newVacant.WorkMethodId}
+                onChange={handleInputChange}
+                required
+              >
+                <option disabled></option>
+                <option value={1}>Presencial</option>
+                <option value={2}>Hibrido</option>
+                <option value={3}>Remoto</option>
+              </FormSelect>
+              <Form.Control.Feedback type="invalid">
+                Selecciona una opcion
+              </Form.Control.Feedback>
+            </FormGroup>
 
-      <div>
-        <ButtonGeneral
-          textButton="Crear vacante"
-          type="submit"
-          handlerClick={handleSubmit}
-        ></ButtonGeneral>
-      </div>
+            <FormGroup as={Col} md="4">
+              <FormLabel className="me-2">Jornada</FormLabel>
+              <FormSelect
+                name="WorkdayId"
+                value={newVacant.WorkdayId}
+                onChange={handleInputChange}
+                required
+              >
+                <option disabled></option>
+                <option value={1}>Tiempo completo</option>
+                <option value={2}>Medio tiempo</option>
+                <option value={3}>Por horas</option>
+              </FormSelect>
+              <Form.Control.Feedback type="invalid">
+                Selecciona una opcion
+              </Form.Control.Feedback>
+            </FormGroup>
+
+            <FormGroup as={Col} md="4">
+              <FormLabel className="me-2">Experiencia</FormLabel>
+              <FormSelect
+                name="SeniorityId"
+                value={newVacant.SeniorityId}
+                onChange={handleInputChange}
+                required
+              >
+                <option disabled></option>
+                {/* <option value={0}>Sin Experiencia</option> */}
+                <option value={4}>Trainee</option>
+                <option value={3}>Junior</option>
+                <option value={2}>Semi-senior</option>
+                <option value={1}>Senior</option>
+              </FormSelect>
+              <Form.Control.Feedback type="invalid">
+                Selecciona una opcion
+              </Form.Control.Feedback>
+            </FormGroup>
+          </Row>
+        </Form>
+        <div>
+          <ButtonGeneral
+            textButton="Crear vacante"
+            type="submit"
+            handlerClick={handleSubmit}
+          ></ButtonGeneral>
+        </div>
       </div>
     </div>
   );
--- client/src/views/MiPerfil/MiPerfil.jsx
+++ client/src/views/MiPerfil/MiPerfil.jsx
@@ -5,12 +5,13 @@ import { useState } from "react";
 import style from "./MiPerfil.module.css"
 import NavBar from "../../components/NavBar/NavBar"
 import { BsFillEnvelopeAtFill, BsFillTelephoneFill, BsGlobeAmericas, BsLinkedin } from 'react-icons/bs'
-// import { PDFViewer, PDFDownloadLink} from "@react-pdf/renderer"
-// import DocuPDF from "../../components/DocuPDF/DocuPDF"
+import { PDFViewer, PDFDownloadLink} from "@react-pdf/renderer"
+import DocuPDF from "../../components/DocuPDF/DocuPDF"
 import ListItem from "../../components/ListItemExperience/ListItemExperience";
 import ListItemStudy from "../../components/ListItemStudy/ListItemStudy";
 import { getUserDetail } from "../../Redux/Actions/actionsFunction/actionsUsers";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const MiPerfil = ({ setValidateState, setCurrentUserStore2 }) => {
@@ -18,7 +19,7 @@ const MiPerfil = ({ setValidateState, setCurrentUserStore2 }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  // const [showPDF, setShowPDF] = useState(false);
+  const [showPDF, setShowPDF] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isLoading2, setIsLoading2] = useState(false);
   
@@ -40,6 +41,8 @@ const MiPerfil = ({ setValidateState, setCurrentUserStore2 }) => {
     pais: '',
     linkedin: '',
     skills: '',
+	plan:'',
+	objetivo:'',
     Experiences:[],
     Formations:[],
     
@@ -72,7 +75,9 @@ const MiPerfil = ({ setValidateState, setCurrentUserStore2 }) => {
         pais: userDetail.Cv?.country,
         linkedin: userDetail.Cv?.linkedin,
         skills: userDetail.Cv?.skill,
-        Experiences:userDetail.Cv?.Experiences,
+		plan: userDetail.PayMethods?.length > 0 ?userDetail.PayMethods[0].Operation?.detail : 'ninguno',
+        objetivo: userDetail.Cv?.educational_institution,
+		Experiences:userDetail.Cv?.Experiences,
         Formations:userDetail.Cv?.Formations
       }));
     }
@@ -87,9 +92,9 @@ const MiPerfil = ({ setValidateState, setCurrentUserStore2 }) => {
   }, []);
 
 
-  // const handleClick = () => {
-  //   setShowPDF(!showPDF);
-  // };
+  const handleClick = () => {
+    setShowPDF(!showPDF);
+  };
 
 
   if(!isLoading2){
@@ -97,12 +102,27 @@ const MiPerfil = ({ setValidateState, setCurrentUserStore2 }) => {
   }
 
   if (!isLoading) {
-    return (<div className={style.container}>
-      <NavBar setValidateState={setValidateState} setCurrentUserStore2={setCurrentUserStore2} ></NavBar>
-      <div style={{ 'margin': '10vh auto' }}>
-        <p style={{ 'margin': '10vh auto', color:'white' }}>No tienes Cv registrada <button onClick={() => navigate('/registro-cv')} > Registar CV</button></p>
-      </div>
-    </div>)
+
+
+	Swal.fire({
+        title: "Opps",
+        text: "Parece que no tienes CV registrada",
+        icon: "warning",
+		preConfirm: () => {
+			navigate('/registro-cv')
+		}
+      });
+
+    // return (<div className={style.container}>
+    //   <NavBar setValidateState={setValidateState} setCurrentUserStore2={setCurrentUserStore2} ></NavBar>
+    //   <div style={{ 'margin': '10vh auto' }}>
+    //     <p style={{ 'margin': '10vh auto', color:'white' }}>No tienes Cv registrada <button onClick={() => navigate('/registro-cv')} > Registar CV</button></p>
+    //   </div>
+    // </div>)
+
+
+
+
   } else {
     return (
 		<>
@@ -174,12 +194,25 @@ const MiPerfil = ({ setValidateState, setCurrentUserStore2 }) => {
 					</div>
 
 					<div className={style.container5}>
+						<h6>Plan: {perfil.plan}</h6>
 						<h4 style={{ "text-align": "left" }}>
 							{perfil.profesion}
 						</h4>
 						<p style={{ "text-align": "justify" }}>
 							{perfil.descripcion}
 						</p>
+					</div>
+
+
+					<div className={style.container5}>
+						<h4 style={{ "text-align": "left" }}>Objetivo laboral</h4>
+						<p>{perfil.objetivo}</p>
+					</div>
+
+
+					<div className={style.container5}>
+						<h4 style={{ "text-align": "left" }}>Skills</h4>
+						<p>{perfil.skills}</p>
 					</div>
 
 					<div className={style.container5}>
@@ -249,12 +282,9 @@ const MiPerfil = ({ setValidateState, setCurrentUserStore2 }) => {
 						</div>
 					</div>
 
-					<div className={style.container5}>
-						<h4 style={{ "text-align": "left" }}>Skills</h4>
-						<p>{perfil.skills}</p>
-					</div>
+					
 
-					{/* {userDetail.Cv &&  <div className={style.container3}>
+					{userDetail.Cv &&  <div className={style.container3}>
 
                 <button
                   style={{ backgroundColor: 'gray' }}
@@ -272,7 +302,7 @@ const MiPerfil = ({ setValidateState, setCurrentUserStore2 }) => {
                   <button style={{ 'backgroundColor': 'gray' }}>Descargar CV en PDF</button>
                 </PDFDownloadLink>
 
-              </div>} */}
+              </div>}
 				</div>
 			</div>
 		</>
