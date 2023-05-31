b6fa9548811e6aa40bdc18532616a70d9a3b1ed3
Xavimont0409 <111645686+Xaviermon@users.noreply.github.com>
2023-05-31 13:23:06 -0500

    solucionando problemas con el validate de empresa y applicant, agregando validacion si el usuario esta baneado o no

--- client/src/views/FormRegistroUsuario/validation.js
+++ client/src/views/FormRegistroUsuario/validation.js
@@ -3,15 +3,18 @@ export const validation = (applicant) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const phoneNumberRegex = /^\+?\d{1,3}[\s-]?\d{1,14}$/;
   const passwordRegex = /^(?=.*[A-Z])(?=.*[\W_])(?!.*\s).{1,10}$/;
+  const regexSimbol = /[\*\!]{1,2}/;
+  const regexMayus = /[A-Z]{1}/;
+  const regexNum = /\d.*\d/;
 
   let errors = {};
   if(applicant.name){
     if(!nameRegex.test(applicant.name)) errors.name = "Nombre no valido"
-    if(applicant.name.length >= 15) errors.name = "Debe tener menos de 15 caracteres"
+    if(applicant.name.length >= 15) errors.name = "Debe tener menos de 20 caracteres"
   }
   if(applicant.lastName){
     if(!nameRegex.test(applicant.lastName)) errors.lastName = "Apellido no valido"
-    if(applicant.lastName.length >= 15) errors.lastName = "Debe tener menos de 15 caracteres"
+    if(applicant.lastName.length >= 15) errors.lastName = "Debe tener menos de 20 caracteres"
   }
   if(applicant.email){
     if(!emailRegex.test(applicant.email)) errors.email = "Email no valido"
@@ -20,7 +23,10 @@ export const validation = (applicant) => {
     if(!phoneNumberRegex.test(applicant.cellphone)) errors.cellphone = "Telefono no valido"
   }
   if(applicant.password){
-    if(!passwordRegex.test(applicant.password)) errors.password = "Contraseña no valida"
+    if(!regexSimbol.test(applicant.password)) errors.password = "Debe tener almenos 1 simbolo"
+    if(!regexMayus.test(applicant.password)) errors.password ="Debe tener una letra mayuscula"
+    if(!regexNum.test(applicant.password)) errors.password = "Debe tener almenos 2 numeros"
+    if(applicant.password.length >= 12) errors.password = "Debe tener menos de 12 caracteres"
   }
   return errors
 };
